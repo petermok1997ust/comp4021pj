b06206d0ba58620a103b906ba5087151c3a857f1
petermok97 <petermok97@gmail.com>
2018-03-28 21:48:47 +0800

    Build skeleton panel

--- js/game.js
+++ js/game.js
@@ -3,4 +3,12 @@ $(function() {
         $("#game-screen").show();
         $("#start-screen").hide();
     });
+    $("#objective-button").click(function(){
+        $("#instruction-canvas").show(500);
+    });
+    $("#close-instruction").click(function(){
+        $("#instruction-canvas").hide(500);
+    });
+
+
 });
