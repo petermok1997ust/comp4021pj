1561943f623443db70065453ea0407548979f797
cflamad <cflamad@users.noreply.github.com>
2018-04-02 13:00:09 +0800

    Generate coins & check conflict of items & count down

--- js/game.js
+++ js/game.js
@@ -9,7 +9,13 @@ var keydown = false;
 var maze = generateMaze();
 var playerXY = [1,1];
 var monsterXY = [2,2];
+var monsters = [];
+var coins = [];
 var moving = false;
+var life = 3;
+var time = 120;
+var score = 0;
+var countDownTaskId;
 
 function generateMaze() {
     return [
@@ -32,24 +38,82 @@ function generateMaze() {
     ];
 }
 
-function afterPlayerMove(x, y) {
-    if(keydown && !moving){
-      movePlayer(x, y);
-    }
-      console.log(x, y);
-}
-
 function generateJQueryEle(type) {
+    var ele;
     switch(type) {
         case '#':
-            return $('<img class="svg" src="img/block.svg"/>');
+            ele = $('<img class="svg" src="img/block.svg"/>');
+            break;
         case '0':
             return;
-        case 'C':
-            return $('<img id="pac-man" class="svg" src="img/pac-man.svg"/>');
+        case 'P':
+            ele = $('<img id="pac-man" class="svg" src="img/pac-man.svg"/>');
+            break;
         case 'M':
-            return $('<img class="ghost svg" src="img/ghost.svg"/>');
+            ele = $('<img class="ghost svg" src="img/ghost.svg"/>');
+            break;
+        case 'C':
+            ele = $('<img class="svg coin" src="img/coin.svg"/>');
+            break;
     }
+    if(ele) {
+        ele.css({
+            width: CELL_WIDTH+'%',
+            height: CELL_HEIGHT+'%',
+        });
+    }
+    return ele;
+}
+
+function occupied(x, y) {
+    return maze[y*NB_COL+x] !== '0';
+}
+
+function generateCoins() {
+    var coins = [];
+    for(var i = 0; i < NB_ROW; i++) {
+        for(var j = 0; j < NB_COL; j++) {
+            if(!occupied(j, i)) {
+                if(Math.random() < 0.3) { // 30% to place a coin here
+                    var ele = generateJQueryEle('C');
+                    ele.css({
+                        left: (j*CELL_WIDTH)+'%',
+                        top: (i*CELL_HEIGHT)+'%'
+                    });
+                    coins.push({
+                        x: j,
+                        y: i,
+                        obj: ele
+                    });
+                }
+            }
+        }
+    }
+    return coins;
+}
+
+function generateMonsters() {
+    var monsters = [];
+    var [x, y] = monsterXY;
+    for(var i = 0; i < NUM_MONSTER;i++) {
+        var ele = generateJQueryEle('M');
+        var color = getRandomColor();
+        // console.log(color);
+        var monsterX = x+i;
+        var monsterY = y+i;
+        ele.css({
+                left: (monsterX*CELL_WIDTH)+'%',
+                top: (monsterY*CELL_HEIGHT)+'%',
+                background: color
+        });
+        monsters.push({
+            x: monsterX,
+            y: monsterY,
+            obj: ele
+        });
+        maze[monsterY*NB_COL+monsterX] = 'M';
+    }
+    return monsters;
 }
 
 function addElementToMap(maze) {
@@ -61,8 +125,6 @@ function addElementToMap(maze) {
             ele.css({
                 left: (x*CELL_WIDTH)+'%',
                 top: (y*CELL_HEIGHT)+'%',
-                width: CELL_WIDTH+'%',
-                height: CELL_HEIGHT+'%',
             });
             mazeDiv.append(ele);
         }
@@ -70,29 +132,23 @@ function addElementToMap(maze) {
 
     // place the player
     var [x, y] = playerXY;
-    var ele = generateJQueryEle('C');
+    var ele = generateJQueryEle('P');
     ele.css({
         left: (x*CELL_WIDTH)+'%',
         top: (y*CELL_HEIGHT)+'%',
-        width: CELL_WIDTH+'%',
-        height: CELL_HEIGHT+'%',
     });
     mazeDiv.append(ele);
 
     // place monster
-    for(var i = 0; i<NUM_MONSTER;i++){
-      var [x, y] = monsterXY;
-      var ele = generateJQueryEle('M');
-      var color = getRandomColor();
-      console.log(color);
-      ele.css({
-          left: ((x+i)*CELL_WIDTH)+'%',
-          top: ((y+i)*CELL_HEIGHT)+'%',
-          width: CELL_WIDTH+'%',
-          height: CELL_HEIGHT+'%',
-          background:color
-      });
-      mazeDiv.append(ele);
+    monsters = generateMonsters();
+    for(var m of monsters) {
+        mazeDiv.append(m.obj);
+    }
+
+    // place coins
+    coins = generateCoins();
+    for(var c of coins) {
+        mazeDiv.append(c.obj);
     }
 }
 
@@ -105,41 +161,27 @@ function getRandomColor() {
   return color;
 }
 
+function gameOver() {
+    clearInterval(countDownTaskId);
+    $(document).off('keydown');
+}
+
+function countDown() {
+    time-=1;
+    $('#current-time').text(time);
+    if(time <= 0) {
+        gameOver();
+    }
+}
+
 function afterShowGameScreen() {
     addElementToMap(maze);
-}
 
-function canMove(x, y) {
-    if(x < 0 || y < 0 || x >= NB_COL || y >= NB_ROW) return false;
-    return maze[NB_COL*y+x] !== '#';
-}
+    $('#current-life').text(life);
+    $('#current-time').text(time);
+    countDownTaskId = setInterval(countDown, 1000);
 
-function movePlayer(newX, newY) {
-    var diff_x = playerXY[0] - newX;
-    var diff_y = playerXY[1] - newY;
-      if(!canMove(newX, newY)) return;
-      moving = true;
-      $('#pac-man').css({
-          left: (newX*CELL_WIDTH)+'%',
-          top: (newY*CELL_HEIGHT)+'%',
-          width: CELL_WIDTH+'%',
-          height: CELL_HEIGHT+'%',
-      });
-      playerXY = [newX, newY];
-      setTimeout(function(){
-        moving = false;
-        afterPlayerMove(newX - diff_x, newY - diff_y);
-      }, MOVE_DURATION);
-
-}
-
-$(function() {
-    $("#start-button").click(function(){
-        $("#game-screen").show();
-        $("#start-screen").hide();
-        afterShowGameScreen();
-    });
-
+    // add control
     $(document).keydown(function(e) {
         var curPos = $('#pac-man').position();
         if(!keydown && ! moving){
@@ -171,6 +213,75 @@ $(function() {
       console.log("keyup");
       if(keydown)
         keydown = false;
+    });
+}
+
+function canMove(x, y) {
+    if(x < 0 || y < 0 || x >= NB_COL || y >= NB_ROW) return false;
+    return maze[NB_COL*y+x] !== '#';
+}
+
+function afterPlayerMove(x, y) {
+    // check conflict
+    for(var i = coins.length-1; i >= 0; i--) {
+        var coin = coins[i];
+        if(coin.x === x && coin.y === y) {
+            coin.obj.animate({ top: ((y-1)*CELL_HEIGHT)+'%' }, 1000)
+            .delay(MOVE_DURATION)
+            .animate({ opacity: 0 }, 500, function() {
+                // remove from DOM after animation
+                coin.obj.remove();
+            });
+            coins.splice(i, 1);
+            score += 10;
+            $('#current-score').text(score);
+        }
+    }
+
+    var player = $('#pac-man');
+    for(var i = monsters.length-1; i >= 0; i--) {
+        var monster = monsters[i];
+        if(monster.x === x && monster.y === y) {
+            onGetDamage();
+        }
+    }
+}
+
+function onGetDamage() {
+    life--;
+    if(life <= 0) gameOver();
+    $('#current-life').text(life);
+}
+
+function movePlayer(newX, newY) {
+    var diffX = playerXY[0] - newX;
+    var diffY = playerXY[1] - newY;
+
+    if(!canMove(newX, newY)) return;
+    moving = true;
+    $('#pac-man').css({
+        left: (newX*CELL_WIDTH)+'%',
+        top: (newY*CELL_HEIGHT)+'%',
+        width: CELL_WIDTH+'%',
+        height: CELL_HEIGHT+'%',
+    });
+    playerXY = [newX, newY];
+    afterPlayerMove(newX, newY);
+    setTimeout(function(){
+        moving = false;
+        
+        if(keydown && !moving){
+            movePlayer(newX - diffX, newY - diffY);
+        }
+    }, MOVE_DURATION);
+
+}
+
+$(function() {
+    $("#start-button").click(function(){
+        $("#game-screen").show();
+        $("#start-screen").hide();
+        afterShowGameScreen();
     });
 });
 
